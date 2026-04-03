7375d34a2d41c6614c936cfdd1473a4f2aa84be4
gabrielseffrin <gabrielfseffrin@gmail.com>
2026-04-03 20:00:05 -0300

    feat: Added token persistence and logout

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,8 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useRouter, usePathname, useRootNavigationState } from 'expo-router';
-import { authService} from "@/services/auth.service";
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import {useRouter, usePathname, useRootNavigationState} from 'expo-router';
+import {authService} from "@/services/auth.service";
 import * as SecureStore from 'expo-secure-store';
+import {is} from "@babel/types";
 
 interface User {
     id: string;
@@ -16,8 +17,9 @@ export function useAuth() {
     return useContext(AuthContext);
 }
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
+export function AuthProvider({children}: { children: React.ReactNode }) {
     const [user, setUser] = useState<User | null>(null);
+    const [isLoading, setLoading] = useState(true);
 
     const pathname = usePathname();
     const router = useRouter();
@@ -26,7 +28,26 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
     useEffect(() => {
 
-        if (!rootNavigationState?.key) return;
+        async function loadUser() {
+            try {
+                const token = await SecureStore.getItemAsync('token');
+                if (token) {
+                    const userData = await authService.getCurrentUser();
+                    setUser(userData);
+                }
+            } catch (error) {
+                console.error("Erro ao carregar usuário:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadUser();
+    }, []);
+
+    useEffect(() => {
+
+        if (!rootNavigationState?.key || isLoading) return;
 
         const inLoginScreen = pathname === '/';
 
@@ -35,11 +56,12 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         } else if (user && inLoginScreen) {
             router.replace('/home');
         }
-    }, [user, router, pathname, rootNavigationState?.key]);
+    }, [user, router, pathname, rootNavigationState?.key, isLoading]);
 
     const signIn = async (username: string, password: string) => {
         try {
-            const { token } = await authService.login({ email: username, password });
+
+            const {token} = await authService.login({email: username, password});
             await SecureStore.setItemAsync('token', token);
             const userData = await authService.getCurrentUser();
             setUser(userData);
@@ -57,12 +79,18 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         }
     };
 
-    const signOut = () => {
-        setUser(null);
+    const signOut = async () => {
+        try {
+            await authService.logout();
+            await SecureStore.deleteItemAsync('token');
+            setUser(null);
+        } catch (error) {
+            console.error("Erro ao fazer logout:", error);
+        }
     };
 
     return (
-        <AuthContext.Provider value={{ user, signIn, signOut }}>
+        <AuthContext.Provider value={{user, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     );
